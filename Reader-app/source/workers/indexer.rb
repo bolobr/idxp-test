5ecfe3fd7e553f29d9c4e6a6bee4b1f0e1c6f78c
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2016-07-04 10:48:22 -0300

    Uploader interface and tests

--- Reader-app/source/workers/indexer.rb
+++ Reader-app/source/workers/indexer.rb
@@ -1,8 +1,15 @@
 require 'sidekiq'
 require 'pathname'
+require 'net/http'
 class Indexer
   include Sidekiq::Worker
   def perform(file_path)
+    a = Indexer.new
+    file_name = Pathname.new(file_path).basename
+    data = a.count_words(file_path)
+    a.upload_results(file_name, data)
+    a.move_file(file_path)
+
   end
 
   def count_words(file_path)
@@ -19,11 +26,21 @@ class Indexer
   end
 
   def upload_results(file_name, file_info)
+    uri = URI.parse("http://localhost:3000/api/v1/receive_data.json")
+    https = Net::HTTP.new(uri.host, uri.port)
+
+    request = Net::HTTP::Post.new(uri.path)
+    request.set_form_data(file_info)
+    response = https.request(request)
+    ##Faking the results while the API is not ready
+    res = {status: 200, msg: "File teste4.txt created"}
+    return res
   end
 
   def move_file(file_path)
+    processing_path = "application_storage/processing_files/#{Pathname.new(file_path).basename}"
     oldfile_path  = "application_storage/old_files/#{Pathname.new(file_path).basename}"
-    FileUtils.mv(file_path, oldfile_path)
+    FileUtils.mv(processing_path, oldfile_path)
     return oldfile_path
   end
 end
