8465ddba9fdb7bdf74b8894837f7201a1675da94
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2016-07-04 13:55:37 -0300

    Routing namespace

--- Backend-app/config/routes.rb
+++ Backend-app/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      #Reader App Interface
+      post '/receive_data' => 'reader_app#receive_data', as: :receive_data
+
+      #Front-end Interface
+      get '/search_term/:term' => 'frontend#search_term', as: :search_term
+
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
