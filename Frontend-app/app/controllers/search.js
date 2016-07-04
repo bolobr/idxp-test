14384dd0c848243afa9b64e578e9506726c03e2b
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2016-07-04 18:23:56 -0300

    Frontend working

--- Frontend-app/app/controllers/search.js
+++ Frontend-app/app/controllers/search.js
@@ -1,17 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  search: function(){
-    console.log(this.get('search_box'))
-    var model = [{
-      file_name: "lula gay",
-      word_count: "3"
-    },
-    {
-      file_name: "lula muito gay",
-      word_count: "5"
+  actions: {
+    search: function(){
+      console.log(this.get('search_box'))
+      var url = "http://localhost:3000/api/v1/search_term/" + this.get('search_box');
+      var that = this;
+      $.ajax(url).then(function(result){
+        console.log(result)
+        var sorted_result = result.files.sort(function(a,b){
+          return a.word_count < b.word_count
+        })
+        that.set('model', sorted_result);
+      })
+      // this.set('model', model);
     }
-  ]
-  this.set('model', model);
-  }.observes("search_box")
+  }
 });
--- Frontend-app/app/routes/search.js
+++ Frontend-app/app/routes/search.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return [{file_name: "teste", word_count: 1}];
+    return [];
   },
 
 });
