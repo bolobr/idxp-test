40badb330bec9d6377e29773e03f005501387b4c
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2016-07-04 10:19:36 -0300

    Reader processador + teste

--- Reader-app/source/workers/indexer.rb
+++ Reader-app/source/workers/indexer.rb
@@ -1,12 +1,29 @@
 require 'sidekiq'
+require 'pathname'
 class Indexer
   include Sidekiq::Worker
   def perform(file_path)
   end
 
-  def count_words
+  def count_words(file_path)
+    file = File.open(file_path, "r")
+    res = {}
+    #Lazy for huge files
+    file.each_line.lazy.each_with_index do |line, i|
+      line_array = line.split
+      line_array.each do |key|
+        res[key] = (res[key] || 0) + 1
+      end
+    end
+    return res
   end
 
-  def upload_results
+  def upload_results(file_name, file_info)
+  end
+
+  def move_file(file_path)
+    oldfile_path  = "application_storage/old_files/#{Pathname.new(file_path).basename}"
+    FileUtils.mv(file_path, oldfile_path)
+    return oldfile_path
   end
 end
--- Reader-app/test/workers/test_indexer.rb
+++ Reader-app/test/workers/test_indexer.rb
@@ -3,15 +3,25 @@ require 'minitest/autorun'
 describe Indexer do
   before do
     @worker = Indexer.new
+    File.open("application_storage/processing_files/teste3.txt", "w") {|f| f.write("teste teste teste") }
+    File.open("application_storage/processing_files/teste4.txt", "w") {|f| f.write("teste teste teste") }
+
   end
 
   describe "When perform is called" do
     it "should count correctly the terms" do
-      skip("Pending")
+      a = {}
+      a["teste"] = 3
+      assert_equal (a), @worker.count_words("application_storage/processing_files/teste3.txt")
     end
 
     it "should upload the info to the database" do
-      skip("Pending")
+      a = {status: 200, msg: "File teste4.txt created"}
+      assert_equal(a), @worker.upload_results 
+    end
+
+    it "should move processed files to old files" do
+      assert_equal "application_storage/old_files/teste4.txt", @worker.move_file("application_storage/processing_files/teste4.txt")
     end
   end
 end
