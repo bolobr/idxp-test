60894573ad7c18b4bda6ae567ce5e7c0ec01894f
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2016-07-05 10:59:35 -0300

    Backend fixing on empty results

--- Backend-app/app/models/term.rb
+++ Backend-app/app/models/term.rb
@@ -4,7 +4,10 @@ class Term
   embeds_many :processed_files
 
   def self.find_all_files(term)
-    a = Term.where(name: term)[0]
-    return a.processed_files
+    a = Term.where(name: term)
+    if a.blank?
+      return []
+    end
+    return a[0].processed_files
   end
 end
