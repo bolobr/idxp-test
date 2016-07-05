fa9568a3393f373df601d13b0215e3a6044a3e30
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2016-07-05 11:01:31 -0300

    Improoving search

--- Frontend-app/app/controllers/search.js
+++ Frontend-app/app/controllers/search.js
@@ -3,14 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     search: function(){
-      console.log(this.get('search_box'));
       var url = "http://localhost:3000/api/v1/search_term/" + this.get('search_box');
       var that = this;
       $.ajax(url).then(function(result){
-
         var sorted_result = result.files.sort(function(a,b){
           return parseInt(a.word_count) < parseInt(b.word_count);
         });
+
         that.set('model', sorted_result);
       });
     }
