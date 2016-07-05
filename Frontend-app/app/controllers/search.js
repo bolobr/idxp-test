3bcebd4b3ee10265bd62223415203c2ededb6545
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2016-07-05 09:48:16 -0300

    Fixing sorting: it was sorting on string and not on int

--- Frontend-app/app/controllers/search.js
+++ Frontend-app/app/controllers/search.js
@@ -3,17 +3,16 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     search: function(){
-      console.log(this.get('search_box'))
+      console.log(this.get('search_box'));
       var url = "http://localhost:3000/api/v1/search_term/" + this.get('search_box');
       var that = this;
       $.ajax(url).then(function(result){
-        console.log(result)
+
         var sorted_result = result.files.sort(function(a,b){
-          return a.word_count < b.word_count
-        })
+          return parseInt(a.word_count) < parseInt(b.word_count);
+        });
         that.set('model', sorted_result);
-      })
-      // this.set('model', model);
+      });
     }
   }
 });
