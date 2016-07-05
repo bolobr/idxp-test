9f1bf16192592916470cc97f37c7f9be87208754
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2016-07-05 10:00:49 -0300

    Comment for folder listener class

--- Reader-app/source/listener_callback/folder_listener_callback.rb
+++ Reader-app/source/listener_callback/folder_listener_callback.rb
@@ -6,16 +6,19 @@ class FolderListenerCallback
 
   end
 
+  ##Simple wrapper for the whole process
   def run(file)
     processing_path = self.move_file(file)
     self.call_job(processing_path)
   end
 
+  ##Insert the job on the queue to be processed
   def call_job(file_path)
     job = Indexer.perform_async(file_path)
     return !job.nil?
   end
 
+  ##Move file around to avoid being processed twice
   def move_file(file_path)
     processing_path  = "application_storage/processing_files/#{Pathname.new(file_path).basename}"
     FileUtils.mv(file_path, processing_path)
